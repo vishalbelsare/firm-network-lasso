e8471a2e82cf5136768d1e5f1820c54319a62f0a
Jesse Tweedle <jesse.tweedle@gmail.com>
2016-10-20 12:07:33 -0400

    Add arguments for parallel testing at work.

--- main_x.r
+++ main_x.r
@@ -1,5 +1,5 @@
 ########################################################################
-# main.R
+# main_x.R
 # Clean and load relevant files
 # License: MIT
 
@@ -7,6 +7,12 @@
 # Jesse Tweedle
 # Oct 18, 2016
 ########################################################################
+
+
+### How to add new equations? 
+### Want industry IO to have a better chance of matching;
+### Hmm. (1) want to make sure industry shares add up?
+### this means...more equations in the pre-process?
 
 library(dplyr)
 library(tibble)
@@ -32,55 +38,36 @@ sourceDir <- function(path, trace = TRUE, ...) {
 
 sourceDir(paste0(getwd(),"/R"))
 
-set.seed(9) 
+#set.seed(9) 
 R <- 20
-N <- 2000
-Z <- 10 # industries
+N <- 1000
+Z <- 100 # industries; each industry must have more than one firm.
 
 args <- initialize_fake_s(R,Z,N)
 
 iz <- args$iz
-# then split on industry rows? apply lasso? etc.
 
 I <- args$I[,1]
 s <- sinit <- args$s
 
-#iz %>% summary() %>% tbl_df() %>% filter(i==1) %>% select(-x,-i)
-
-# z <- 2 # industry 1, to start.
-# sz <- s[iz[z,]==1] # select
-# cz <- sz
-# z_len <- length(sz)
-# 
-# I_mc <- I %>% t() %>% list() %>%  rep(z_len) %>% bdiag()
-# s_mc <- s %>% t() %>% list() %>%  rep(z_len) %>% bdiag()
-# X_mc <- cbind(I_mc,s_mc)
-# 
-# X_mc <- c(I,s) %>% t() %>% list() %>%  rep(z_len) %>% bdiag()
-# fit <- glmnet(X_mc,sz,intercept=FALSE,lower.limits=0,upper.limits=1)
-# y <- coef(fit)
-# # # pick one with max df.
-# ncoef <- which(fit$df==max(fit$df))[1]
-# yz <- y %>% summary() %>% tbl_df() %>% filter(j==ncoef) %>% select(-j,-x) %>% mutate(i=i-1)
-# nnz <- dim(yz)[1]
-# yzs <- sparseMatrix(i=yz$i,j=rep_len(1,nnz),x=1,dims=c((R+N)*z_len,1))
-#can either return this directly, or process it to save a and g?
-
-# X_mc <- (X_mc %*% (yzs %>% to_sdiag())) %>% summary() %>% tbl_df() %>% filter(x>0)
-
-glmnet_z <- function(z,R,N) {
+# pass in args.
+glmnet_z <- function(z,args) {
   gc()
   print(str_c("industry: ", z))
+  
+  I <- args$I
+  iz <- args$iz
+  s <- args$s
+  R <- args$R
+  N <- args$N
+  
   sz <- s[iz[z,]==1] # select 
   cz <- sz
   
   z_len <- length(sz)
   
   X_mc <- c(I,s) %>% t() %>% list() %>%  rep(z_len) %>% bdiag()
-  # I_mc <- I %>% t() %>% list() %>%  rep(z_len) %>% bdiag()
-  # s_mc <- s %>% t() %>% list() %>%  rep(z_len) %>% bdiag()
-  # X_mc <- cbind(I_mc,s_mc)
-  
+
   fit <- glmnet(X_mc,sz,alpha=0.95,nlambda=100,intercept=FALSE,lower.limits=0,upper.limits=1)
   y <- coef(fit)
   # pick one with max df.
@@ -93,16 +80,15 @@ glmnet_z <- function(z,R,N) {
   X_mc <- X_mc %>% df_to_s(dims=c(z_len,(R+N)*z_len))
 
   X_ag <- Reduce(cbind, .sparseDiagonal(R+N) %>% list() %>% rep(z_len))
-  # this is A and G together.
-#  X_ag %>% str()
-  # ok this works
   X_ag <- X_ag %*% (yzs %>% to_sdiag()) %>% summary() %>% tbl_df() %>% filter(x>0) %>% df_to_s(dims=c(R+N,(R+N)*z_len))
   
   return(list(y_z=yzs,X_mc=X_mc,X_ag=X_ag))
 }
 
+preprocess_args <- list(I=I,iz=iz,s=s,R=R,N=N)
 # number of industries = Z. might have to keep I_mc, etc, outside the function?
-res <- lapply(1:Z,FUN=glmnet_z,R=R,N=N)
+res <- lapply(1:Z,FUN=glmnet_z,args=preprocess_args)
+gc()
 
 # Z = length(res)
 somey <- unlist(res)[seq(1,3*Z,3)]
@@ -114,19 +100,11 @@ yyy <- Reduce(rbind,somey)
 X_mc <- unlist(res)[seq(2,3*Z,3)] %>% bdiag()
 X_ag <- Reduce(cbind, unlist(res)[seq(3,3*Z,3)]) #%>% str() #%>% bdiag()
 
-# so here, either do cbind or bdiag, I think cbind.
-# I think c.
-# ok, sure, but do this by z.
-# X_ag <- Reduce(cbind, .sparseDiagonal(R+N) %>% list() %>% rep(N))
-# # this is A and G together.
-# 
-# # ok this works
-# X_ag <- X_ag %*% (yyy %>% to_sdiag()) %>% summary() %>% tbl_df() %>% filter(x>0) %>% df_to_s(dims=c(R+N,(R+N)*N))
-
 cz_list <- 
   lapply(1:Z,FUN=function(z) {
     s[iz[z,]==1]
   })
+
 szs <- Reduce(c,cz_list)
 c_mc <- szs
 c_a <- rep_len(1,R)
@@ -134,17 +112,6 @@ c_g <- 1-args$beta
 
 c <- c(c_mc,c_a,c_g)
 X <- rbind(X_mc,X_ag)
-
-###### DO THIS NEXT. APPLY FIRST STEP ZEROS TO THESE MATRICES.
-# X_ari <- Reduce(cbind, .sparseDiagonal(R) %>% list() %>% rep(N))
-# X_gij <- Reduce(cbind, .sparseDiagonal(N) %>% list() %>% rep(N))
-# 
-# X <- rbind(X_mc,bdiag(X_ari,X_gij))
-
-# c_ari <- rep_len(1,R)
-# c_gij <- 1-args$beta
-# 
-# c <- c(c_mc,c_ari,c_gij)
 
 # deviance for some reason can go above 100%? I just want it to be as high as possible.
 glmnet.control(devmax = 5) 
@@ -154,42 +121,24 @@ glmnet.control(devmax = 5)
 #,lower.limits=0,upper.limits=1) <- may or may not need this.
 fit <- glmnet(X,c,alpha=1,nlambda=100,intercept=FALSE,lower.limits=0,upper.limits=1)
 y <- coef(fit)
-# print(fit$df)
-# print(fit)
 
-# next, rearrange shit to get back A, keeping in mind the re-organization of s via z.
-# last one is to get rid of intercept index.
 yx <- y %>% summary() %>% tbl_df() %>% filter(j==max(j)) %>% mutate(i=i-1,j=1)
 yx <- yx %>% df_to_s(dims=c((R+N)*N,1))
 nnz <- fit$df[length(fit$df)] / (R*N+N^2)
 print(str_c("sparsity: ",nnz))
 
 #y---first R are a_{.1}, next N are g_{.1}, etc. so split by R+N.
-#yx %>% matrix(nrow=R+N,ncol=N,byrow=FALSE)
+# this works like magic.
 dim(yx) <- c(R+N,N)
 
 A <- yx[1:R,1:N]
 G <- yx[(R+1):(R+N),1:N]
-
-# doesn't work, because now they split up like a,g,a,g,a,g,etc.
-# ok, cool. now it's like (a,g)_1,(a,g)_2, etc. but 1,2,...are organized by industry.
-
-# Rearrange y to get A
-# y_sa <- yx %>% filter(i<=R*N) %>% mutate(col=floor((i-1)/R)+1,row=i-(col-1)*R)
-# A <- sparseMatrix(i=y_sa$row,j=y_sa$col,x=y_sa$x,dims=c(R,N))
-# 
-# # Rearrange y to get G
-# y_ga <- yx %>% filter(i>R*N) %>% mutate(i=i-R*N) %>% mutate(col=floor((i-1)/N)+1,row=i-(col-1)*N)
-# G <- sparseMatrix(i=y_ga$row,j=y_ga$col,x=y_ga$x,dims=c(N,N))
-
-# Do A and G satisfy the equations? #sx <- solve_s(R,N,list(beta=args$beta,ir=args$ir,A=A,G=G))
 
 # Solve for implied s
 s_hat <- t(A) %*% (I %>% matrix(nrow=R,ncol=1)) + t(G) %*% (((1-args$beta)*s) %>% matrix(nrow=N,ncol=1)) 
 df <- tibble(s_hat=s_hat[,1],s=szs)
 lm(s_hat %>% log() ~ s %>% log(),data=df %>% filter(s_hat>0)) %>% summary()
 ggplot(df %>% filter(s_hat>0),aes(x=s,y=s_hat))+geom_point(alpha=0.5) + scale_x_log10() + scale_y_log10() 
-
 
 # How does it do for big firms (> median size).
 q <- quantile(df$s,probs=0.5)
